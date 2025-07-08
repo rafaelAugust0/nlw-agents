a8388fc92c2323e40d1d06d204a69fff4354adb0
rafaelAugust0 <rafael.augustoo1612@gmail.com>
2025-07-08 15:51:07 -0300

    environment creation and initial configs

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import {fastify} from 'fastify';
+import {
+    serializerCompiler,
+    validatorCompiler,
+    ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+import {fastifyCors} from '@fastify/cors'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+    origin: '*',
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+const PORT = process.env.PORT || 3333;
+
+app.get('/health', () => {
+    return 'OK'
+})
+
+app.listen({ port: Number(PORT)}).then(() => {
+    console.log("HTTP SERVER RUNNING!");
+});
